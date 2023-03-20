89818cf279809d37f2d1e19fe3ad7056455ec5f6
Hasanat902 <abulhasanat902@gmail.com>
2023-03-20 12:51:10 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 
 function App() {
   return (
-    <div>
-      <h2>Hello World</h2>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/hotels" element={<List></List>}></Route>
+      <Route path="/hotels/:id" element={<Hotel></Hotel>}></Route>
+    </Routes>
   );
 }
 
