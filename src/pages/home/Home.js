f06a98d519dd180b31e89413d7b8334161a3579c
Hasanat902 <abulhasanat902@gmail.com>
2023-03-20 14:30:18 +0600

    added navbar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <Navbar></Navbar>
         </div>
     );
 };
